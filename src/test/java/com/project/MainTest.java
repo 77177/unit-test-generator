5470655f8cff8b8fd78300b7804f4be5280d1b33
Stas Tarasov <iluvstrwrs@mail.ru>
2020-02-22 10:46:38 +0300

    pom.xml

---
+++ src/test/java/com/project/MainTest.java
@@ -0,0 +1,15 @@
+package com.project;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+class MainTest {
+
+    @Test
+    public void test() {
+        Assert.assertEquals(1,0);
+    }
+
+}
