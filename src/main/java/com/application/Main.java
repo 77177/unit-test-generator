e913e6a20b90dd472ff8b55ba8d71eb482b4ee74
Stas Tarasov <iluvstrwrs@mail.ru>
2020-03-25 18:02:52 +0300

    added comments for task

--- src/main/java/com/application/Main.java
+++ src/main/java/com/application/Main.java
@@ -6,11 +6,22 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-     ClassScanner sc = new ClassScanner();
-     sc.scanPath();
-     List<Class> listClass = sc.getScannedClasses();
-     for (Class cl : listClass){
-         System.out.println(cl.getName());
-     }
+        ClassScanner sc = new ClassScanner();
+        sc.scanPath();
+        List<Class> listClass = sc.getScannedClasses();
+        for (Class cl : listClass) {
+            System.out.println(cl.getName());
+        }
+
+        // взять классы и с помошью reflection создать заглушки для теста по типу
+
+        //public class ClassNameTest {
+        //        @Test
+        //        public void MethodNameTest{
+        //
+        //        }
+        //}
+
+
     }
 }
