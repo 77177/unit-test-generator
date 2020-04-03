d8e92c80a00287cd4f58316a9111db5f4fcc116c
Stas Tarasov <iluvstrwrs@mail.ru>
2020-04-03 16:20:52 +0300

    added tasks

--- src/main/java/com/application/Main.java
+++ src/main/java/com/application/Main.java
@@ -1,11 +1,16 @@
 package com.application;
 
 import com.application.classScanner.ClassScanner;
+import com.application.examples.Triangle;
+import com.application.testDataGenerator.TestDataGenerator;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         ClassScanner sc = new ClassScanner();
         sc.scanPath();
         List<Class> listClass = sc.getScannedClasses();
@@ -13,31 +18,44 @@ public class Main {
             System.out.println(cl.getName());
         }
 
-        // взять классы и с помошью reflection создать заглушки для теста по типу
+        List<String> classes = new LinkedList<String>();
+        String ls = System.lineSeparator();
+        for (Class cl : listClass) {
+            String classDesc = "public class " + cl.getSimpleName() + "Test {" + ls;
+            List<String> methods = new LinkedList<String>();
+            for (Method m : cl.getDeclaredMethods()) {
+                String methodDesc = "\t@Test" + ls;
+                methodDesc += "\tpublic void " + m.getName() + "Test {" + ls;
+                methodDesc += "\t\t" + ls;
+                methodDesc += "\t}" + ls;
+                methods.add(methodDesc);
+            }
+            classDesc += String.join(ls, methods);
+            classDesc += "}" + ls;
+            classes.add(classDesc);
+        }
+        System.out.println(String.join(ls, classes));
 
-        //public class ClassNameTest {
-        //        @Test
-        //        public void MethodNameTest{
-        //
-        //        }
-        //}
+        TestDataGenerator testDataGenerator = new TestDataGenerator();
 
+        System.out.println(testDataGenerator.getTestObject(String.class));
         // добавить в сгенерированную заглушку логику теста
+
+        //1) создаем заглушку +
+        //2) взять параметры метода, который мы тестируем (updateCommune)
+        //3) на основе параметров метода создать логику создания обьектов данного классаю (final Commune group = new Commune())
+        //4) после того, как создана логика создания обьектов -> вызвать метод с созданными аргументами. (final Long result = communeControllerUnderTest.updateCommune(group);)
+
 
 //        @Test
 //        void testUpdateCommune() {
 //            // Setup
 //            final Commune group = new Commune();
-//            //когда класс mockGroupService вызывает метод updateCommune с любым аргументом класса Commune, тогда возвращать 0L.
-//            when(mockGroupService.updateCommune(any(Commune.class))).thenReturn(0L);
 //
 //            // Run the test
-//            final Long result = communeControllerUnderTest.updateCommune(group);
+//            communeControllerUnderTest.updateCommune(group);
 //
-//            // Verify the results
-//            assertEquals(0L, result);
-//        }
-
-
+//
+//         }
     }
 }
